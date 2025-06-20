a3a074079997d82835668a9f357c4a1559eefa14
kimdev <kim__dev@hufs.ac.kr>
2025-06-20 21:38:29 +0900

    feat: 일반 사용자 홈 페이지 컨트롤러 기능 추가

--- src/main/java/roomescape/view/NormalController.java
+++ src/main/java/roomescape/view/NormalController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class NormalController {
 
+    @GetMapping
+    public String normalHomePage() {
+        return "index";
+    }
+
     @GetMapping("/reservation")
     public String normalReservationPage() {
         return "reservation";
